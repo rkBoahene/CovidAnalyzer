1c683d58d82428d38b64ac3c8c967f526ba12eca
Richmond Boahene <rkboahene2@gmail.com>
2021-08-12 23:11:32 +0000

    add sqlite database conection

---
+++ covidanalyser/schema.sql
@@ -0,0 +1,16 @@
+DROP TABLE IF EXISTS user;
+DROP TABLE IF EXISTS research;
+
+CREATE TABLE user(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT UNIQUE NOT NULL,
+    password TEXT NOT NULL
+)
+
+CREATE TABLE research(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    researcher_id  INTEGER NOT NULL,
+    created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    info_gathered TEXT NOT NULL,
+    FOREIGN KEY (researcher_id) REFERENCES user (id)
+)
